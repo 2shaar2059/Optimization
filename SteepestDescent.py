438cc7a9b02f507551fb7a4332d039931a0affa6
2shaar2059 <tushaar.jain.duke@gmail.com>
2022-06-02 01:05:25 -0500

    added better plotting

--- SteepestDescent.py
+++ SteepestDescent.py
@@ -7,49 +7,44 @@ from mpl_toolkits.mplot3d import Axes3D
 # ====================================================================
 # start of the code that is custom to each optimization problem
 
-# M = np.array([[1.0, 2.0],
-#               [3.0, 4.0],
-#               [5.0, 6.0]])
-#
-# y = np.array([[7.0],
-#               [8.0],
-#               [9.0]])
+M = np.array([[0.1, 0.2],
+              [0.3, 0.4],
+              [0.5, 0.6]])
 
-M = np.array([[1.0, 2.0],
-              [3.0, 4.0],
-              [5.0, 6.0]])
-
-y = np.array([[7.0],
-              [8.0],
-              [9.0]])
-
-
+y = np.array([[0.7],
+              [0.8],
+              [0.1]])
 
 # initial guess
-x0 = np.array([[0.0],
-               [0.0]])
+x0 = np.array([[2.0],
+               [4.0]])
 
 """once the norm of the residual falls below this tolerance, stop optimizing"""
-RESIDUAL_TOLERANCE = 1e-8
+RESIDUAL_TOLERANCE = 1e-10
+
+MAX_ITERATIONS = 100
 
 # end of the code that is custom to each optimization problem
 # ====================================================================
 
 
-b = M.T @ y  # TODO REPLACE ME
-A = M.T @ M  # TODO REPLACE ME
+b = M.T @ y
+A = M.T @ M
 
 
 # The quadratic loss function whose minimum occurs at the same x that minimizes the residual of the original Mx=y
 # system in the L2 sense. Evaluating this is not necessary for computing the minimum but rather for seeing into how
 # the algorithm works under the hood (curiosity)
 def f(x):
-    return 0.5 * x.T @ A @ x - b.T @ x
+    # return (0.5 * x.T @ A @ x - b.T @ x)[0][0]
+    # return np.linalg.norm(M @ x - y, 2)
+    y_residual = M @ x - y
+    return (y_residual.T @ y_residual)[0][0]
 
 
-def visualizeLoss(min_x1, max_x1, min_x2, max_x2):
-    x1_arr = np.linspace(min_x1, max_x1, 50)
-    x2_arr = np.linspace(min_x2, max_x2, 50)
+def visualize_f(min_x1, max_x1, min_x2, max_x2):
+    x1_arr = np.linspace(min_x1, max_x1, 20)
+    x2_arr = np.linspace(min_x2, max_x2, 20)
 
     X, Y = np.meshgrid(x1_arr, x2_arr)
     Z = np.zeros(np.shape(X))
@@ -59,26 +54,23 @@ def visualizeLoss(min_x1, max_x1, min_x2, max_x2):
 
     ax = plt.figure().gca(projection=Axes3D.name)
     ax.plot_surface(X, Y, Z, cmap='viridis')
-    ax.set_title('Loss')
+    ax.set_title('f(x)')
     ax.set_xlabel('x1')
     ax.set_ylabel('x2')
-    ax.set_zlabel('loss')
+    ax.set_zlabel('f')
 
 
-def calculate_residual(x):
-    return b - A @ x
-
-
-def SteepestDescent(max_iterations):
+def SteepestDescent():
     current_x = x0
-    residual = calculate_residual(current_x)
+    residual = b - A @ current_x
     residual_norm = np.linalg.norm(residual)
     iterations = 0
 
-    residual_norm_arr = [residual_norm]
+    b_residual_norm_arr = [residual_norm]
+    f_arr = [f(current_x)]
     iteration_arr = [0]
 
-    while iterations < max_iterations and residual_norm > RESIDUAL_TOLERANCE:
+    while iterations < MAX_ITERATIONS and residual_norm > RESIDUAL_TOLERANCE:
         print("Iteration:", iterations)
 
         gradient = (-residual).T
@@ -86,17 +78,27 @@ def SteepestDescent(max_iterations):
 
         print("residual:", residual)
         print()
+
         current_x += step_size * gradient.T
-        residual = calculate_residual(current_x)
-        residual_norm = np.linalg.norm(residual)
+        residual = b - A @ current_x
+        residual_norm = np.linalg.norm(residual, 2)
         iterations += 1
 
-        residual_norm_arr.append(residual_norm)
+        b_residual_norm_arr.append(residual_norm)
+        f_arr.append(f(current_x))
         iteration_arr.append(iterations)
 
-    plt.plot(iteration_arr, residual_norm_arr)
-    plt.xlabel("iteraions")
-    plt.ylabel("norm of residual")
+    plt.subplot(2, 1, 1)
+    plt.plot(iteration_arr, b_residual_norm_arr, "-o")
+    plt.xlabel("iterations")
+    plt.ylabel("norm of b residual")
+    plt.yscale("log")
+    plt.grid(True)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(iteration_arr, f_arr, "-o")
+    plt.xlabel("iterations")
+    plt.ylabel("f(x)")
     plt.yscale("log")
     plt.grid(True)
 
@@ -105,8 +107,8 @@ def SteepestDescent(max_iterations):
 
 
 if __name__ == "__main__":
-    optimized_guess, iterations_taken = SteepestDescent(10)
+    optimized_guess, iterations_taken = SteepestDescent()
     print("Took", iterations_taken, "iterations")
     print("optimized_guess: ", optimized_guess)
-    visualizeLoss(-10, 10, -10, 10)
+    visualize_f(-10, 10, -10, 10)
     plt.show()
