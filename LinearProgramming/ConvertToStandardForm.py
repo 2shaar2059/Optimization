dd458478c7d1bab6c04d80720b220286c9bda934
Tushaar <tushaar.jain.duke@gmail.com>
2022-12-14 01:37:27 -0500

    added 2nd faster way to construct non-negativity constraint. also added some docstrings

--- LinearProgramming/ConvertToStandardForm.py
+++ LinearProgramming/ConvertToStandardForm.py
@@ -11,33 +11,49 @@ class Objective:
 
 
 class Constraint:
-    def __init__(self, constraint_expression):
-        (self.coeffs, self.decision_vars, self.constraint_type,
-         self.right_hand_side) = parse_constraint(constraint_expression)
+    def __init__(self, constraint_expression=None, slack_name=None):
+        if slack_name:
+            self.coeffs = [1.0]
+            self.decision_vars = [slack_name]
+            self.constraint_type = '>='
+            self.right_hand_side = [0.0]
 
-    def is_non_negativity(self):
+        else:
+            (self.coeffs, self.decision_vars, self.constraint_type,
+             self.right_hand_side) = parse_constraint(constraint_expression)
+
+    def __is_non_negativity(self):
         return (self.constraint_type == '>=' and
                 self.coeffs == [1.0] and
                 self.right_hand_side in [0.0, -0.0])
 
-    def is_equality(self):
+    def __is_equality(self):
         return self.constraint_type == '='
 
-    def make_greater_than(self):
+    def needs_slack_variable(self):
+        """True if this is an inequality constraint which doesnt represent a non-negativity constraint
+        """
+        return not self.__is_equality() and not self.__is_non_negativity()
+
+    def __make_greater_than_or_equal(self):
+        """Convert from less-than-or-equal-to constraint to greater-than-or-equal-to constraint
+        """
         if(self.constraint_type == '<='):  # need to flip constraint
             self.coeffs = [-coeff for coeff in self.coeffs]
             self.right_hand_side *= -1
             self.constraint_type = '>='
 
-    def needs_slack_variable(self):
-        return not self.is_non_negativity() and not self.is_equality()
+    def convert_to_equality(self, slack_var_name):
+        """Convert to an equality constraint with a slack variable whose name is given as input parameter
 
-    def make_equality(self, slack_var_name):
-        self.make_greater_than()
+        Args:
+            slack_var_name (str): name of slack variable to introduce
+        """
+        self.__make_greater_than_or_equal()
         if(self.constraint_type == '>='):
             self.decision_vars.append(slack_var_name)
             self.coeffs.append(-1.0)
-            self.constraint_type == '='
+            self.constraint_type = '='
 
 
 class LinearProgram:
@@ -69,8 +85,8 @@ class LinearProgram:
         for constraint in self.constraints:
             if constraint.needs_slack_variable():
                 new_slack_var = f"slack_{slack_var_count}"
-                constraint.make_equality(new_slack_var)
-                slack_var_constraints.append(Constraint(f"{new_slack_var}>=0"))
+                constraint.convert_to_equality(new_slack_var)
+                slack_var_constraints.append(Constraint(slack_name=new_slack_var))
                 slack_var_count += 1
         self.constraints += slack_var_constraints
         print("F")
