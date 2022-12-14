c377322f3d476ef57447ae01bbd0ef12f90bc3c1
Tushaar <tushaar.jain.duke@gmail.com>
2022-12-14 01:11:00 -0500

    added slack variable support

--- LinearProgramming/ConvertToStandardForm.py
+++ LinearProgramming/ConvertToStandardForm.py
@@ -1,10 +1,114 @@
-problem = "max 3x_1 - 2x_2 - x_3 + x_4 - 87"
-constraints = [
-    "4x_1 - x_2 + x_4 <= 6",
-    "-7x_1 + 8x_2 + x_3 >= 7",
-    "x_1 + x_2 + 4x_4 = 12",
-    "x_1  >= 0",
-    "x_2  >= 0",
-    "x_3  >= 0",
-]
+from functools import reduce
+import operator
 
+import numpy as np
+from LPparser import *
+
+
+class Objective:
+    def __init__(self, objective_expression):
+        (self.min_or_max, self.coeffs, self.decision_vars) = parse_objective(objective_expression)
+
+
+class Constraint:
+    def __init__(self, constraint_expression):
+        (self.coeffs, self.decision_vars, self.constraint_type,
+         self.right_hand_side) = parse_constraint(constraint_expression)
+
+    def is_non_negativity(self):
+        return (self.constraint_type == '>=' and
+                self.coeffs == [1.0] and
+                self.right_hand_side in [0.0, -0.0])
+
+    def is_equality(self):
+        return self.constraint_type == '='
+
+    def make_greater_than(self):
+        if(self.constraint_type == '<='):  # need to flip constraint
+            self.coeffs = [-coeff for coeff in self.coeffs]
+            self.right_hand_side *= -1
+            self.constraint_type = '>='
+
+    def needs_slack_variable(self):
+        return not self.is_non_negativity() and not self.is_equality()
+
+    def make_equality(self, slack_var_name):
+        self.make_greater_than()
+        if(self.constraint_type == '>='):
+            self.decision_vars.append(slack_var_name)
+            self.coeffs.append(-1.0)
+            self.constraint_type == '='
+
+
+class LinearProgram:
+    def __init__(self, objective, constraints):
+        self.objective: Objective = Objective(objective)
+        self.constraints: list[Constraint] = [Constraint(constraint) for constraint in constraints]
+
+        objective_vars = set(self.objective.decision_vars)
+        constraint_vars = set().union(*[constraint.decision_vars for constraint in self.constraints])
+
+        extraneous_vars = constraint_vars - objective_vars
+        if extraneous_vars != set():
+            raise Exception(
+                f"{extraneous_vars} found in constraints but not in objective")
+
+        # ensure decision variable naming con't match with slack and artifical variable naming
+        # for var in objective_vars:
+        #     assert(
+        #         'slack_' not in var), f"Decision variable {var} cannot start with \"slack_\" since that naming is reserved for slack variables"
+        #     assert(
+        #         'artif_' not in var), f"Decision variable {var} cannot start with \"artif_\" since that naming is reserved for artificial variables"
+
+        unconstrained_vars = objective_vars - constraint_vars
+        if unconstrained_vars != set():
+            print(f"Found Unconstrianed Variables: {unconstrained_vars}")
+
+        slack_var_constraints = []
+        slack_var_count = 0
+        for constraint in self.constraints:
+            if constraint.needs_slack_variable():
+                new_slack_var = f"slack_{slack_var_count}"
+                constraint.make_equality(new_slack_var)
+                slack_var_constraints.append(Constraint(f"{new_slack_var}>=0"))
+                slack_var_count += 1
+        self.constraints += slack_var_constraints
+        print("F")
+        # for unconstrained_var in unconstrained_vars:
+        # introduce artifical variable
+
+
+"""
+inequality constriants: ax >= b
+ax >= b
+ax - b >= 0
+ax - b = x'
+
+x' >= 0
+ax - x' = b
+
+
+inequality constriants: ax <= b
+-ax >= -b
+
+x' >= 0
+-ax - x' = -b
+"""
+
+
+if __name__ == "__main__":
+    # objective = "max 3x_1 - 2x_2 - x_3 + x_4"
+    objective = "max 3x_1 - 2x_2 - x_3 + x_4 + arti"
+    constraints = [
+        "4x_1 - x_2 + x_4 <= 6",
+        "-7x_1 + 8x_2 + x_3 >= 7",
+        "x_1 + x_2 + 4x_4 = 12",
+        "x_1  >= 0",
+        "x_2  >= 0",
+        "x_3  >= 0",
+
+        # "_xsdf  >= 0",
+        # "sdfx  >= 0",
+    ]
+
+    lpp = LinearProgram(objective, constraints)
--- LinearProgramming/LPparser.py
+++ LinearProgramming/LPparser.py
@@ -1,21 +1,19 @@
 from enum import Enum
-import numpy as np
 
 
 def parse_linear_expression(expression):
-    #remove spaces from expression 
+    # remove spaces from expression
     expression = "".join(expression.split(" "))
 
-
-    #parser state declarations
+    # parser state declarations
     PARSING_COEFFICIENT = 1
-    PARSING_DECSION_VARIABLE = 2
+    PARSING_DECISION_VARIABLE = 2
 
     curr_state = PARSING_COEFFICIENT
     coeffs = []
-    descion_vars = []
+    decision_vars = []
     coeff = ""
-    descion_var = ""
+    decision_var = ""
     for char in expression:
         numeric = char.isnumeric()
         negative = (char == '-')
@@ -25,83 +23,63 @@ def parse_linear_expression(expression):
         next_state = curr_state
 
         if curr_state == PARSING_COEFFICIENT:
-            if not (numeric or add or negative): # done parsing coefficent, start parsing descion variable
-                descion_var += char
-                if(coeff == ""): coeff = "1"
-                elif(coeff == "-"): coeff = "-1"
-                elif(coeff == "+"): coeff = "1"
+            if not (numeric or add or negative):  # done parsing coefficent, start parsing decision variable
+                decision_var += char
+                if(coeff == ""):
+                    coeff = "1"
+                elif(coeff == "-"):
+                    coeff = "-1"
+                elif(coeff == "+"):
+                    coeff = "1"
                 coeffs.append(float(coeff))
-                coeff = "" #reset coefficient because it has been completely parsed
-                next_state = PARSING_DECSION_VARIABLE
-            else: #not done parsing coefficent
-                coeff+=char
-        elif curr_state == PARSING_DECSION_VARIABLE:
-            if(add or negative): # done parsing descion variable, start parsing coefficent
+                coeff = ""  # reset coefficient because it has been completely parsed
+                next_state = PARSING_DECISION_VARIABLE
+            else:  # not done parsing coefficent
                 coeff += char
-                descion_vars.append(descion_var)
-                descion_var = "" #reset descion_var because it has been completely parsed
+        elif curr_state == PARSING_DECISION_VARIABLE:
+            if(add or negative):  # done parsing decision variable, start parsing coefficent
+                coeff += char
+                decision_vars.append(decision_var)
+                decision_var = ""  # reset decision_var because it has been completely parsed
                 next_state = PARSING_COEFFICIENT
             else:
-                descion_var += char
+                decision_var += char
         curr_state = next_state
 
-    #should've ended trying to parse a decsion variable
-    descion_vars.append(descion_var)    
-    assert len(coeffs) == len(descion_vars)
-    
-    return (coeffs, descion_vars)
+    # should've ended trying to parse a decision variable
+    decision_vars.append(decision_var)
+    assert len(coeffs) == len(decision_vars)
+
+    return (coeffs, decision_vars)
 
 
-def parse_problem(problem):
-    #remove spaces from problem 
-    problem = "".join(problem.split(" "))
-    
-    min_or_max = None
-    min_or_max = "min" if "min" in problem else "max" if "max" in problem else None
+def parse_objective(objective):
+    min_or_max = "min" if "min" in objective else "max" if "max" in objective else None
     assert(min_or_max in ["min", "max"])
-
-    #remove min or max from problem
-    expression = problem.strip(min_or_max)
-    assert(min_or_max not in ["min" or "max"])
-
+    expression = objective.strip(min_or_max)  # remove min or max from objective
     return (min_or_max, *parse_linear_expression(expression))
 
 
-def parse_constraints(constraints):
-    constraint_types = [">=", "<=", "="]
-    
-    parsed_constaints = []
-    for constraint in constraints:
-        #remove spaces from constraint 
-        constraint = "".join(constraint.split(" "))
+def parse_constraint(constraint_expression):
+    constraint_expression = "".join(constraint_expression.split(" "))  # remove spaces from constraint_expression
 
-        expression = None
-        right_hand_side = None
-        constraint_type = None
-        for constraint_type in constraint_types: 
-            if constraint_type in constraint:
-                expression, right_hand_side = constraint.split(constraint_type)
-                break
-        if constraint_type == None:
-            raise Exception("Constraint was neither an inequality nor equality") 
-        
-        (coeffs, descion_vars) = parse_linear_expression(expression) 
-        right_hand_side = float(right_hand_side)
-        parsed_constaints.append((coeffs, descion_vars, constraint_type, right_hand_side))
-    return parsed_constaints
+    expression = None
+    right_hand_side = None
+    constraint_type = None
+    for constraint_type in [">=", "<=", "="]:
+        if constraint_type in constraint_expression:
+            expression, right_hand_side = constraint_expression.split(constraint_type)
+            break
+    if constraint_type == None:
+        raise Exception("Constraint was neither an inequality nor equality")
 
-class LPparser:
-    def __init__(self, problem, constraints):
-        self.problem = parse_problem(problem)
-        self.constraints = parse_constraints(constraints)
-        # self.decsion_vars = []
-        # self.A = None
-        # self.b = None
-        # self.c = None
+    (coeffs, decision_vars) = parse_linear_expression(expression)
+    right_hand_side = float(right_hand_side)
+    return coeffs, decision_vars, constraint_type, right_hand_side
 
 
 if __name__ == "__main__":
-    problem = "max 3x_1 - 2x_2 - x_3 + x_4"
+    objective = "max 3x_1 - 2x_2 - x_3 + x_4"
     constraints = [
         "4x_1 - x_2 + x_4 <= 6",
         "-7x_1 + 8x_2 + x_3 >= 7",
@@ -110,5 +88,6 @@ if __name__ == "__main__":
         "x_2  >= 0",
         "x_3  >= 0",
     ]
-
-    lpp = LPparser(problem,constraints)
+    parsed_objective = parse_objective(objective)
+    parsed_constraints = parse_constraints(constraints)
+    print("done")
