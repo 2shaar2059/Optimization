695e65c26e4e08141cbfcb249514b75d3d1ea739
Tushaar <tushaar.jain.duke@gmail.com>
2023-06-24 23:56:24 -0700

    fixed classic gram schmidt to be the truely inferior (accidentally implemented modified gram schmidt)

--- Factorizations/QR/ClassicGramSchmidt.py
+++ Factorizations/QR/ClassicGramSchmidt.py
@@ -5,18 +5,16 @@ def ClassicGramSchmidt(A):
     rows, cols = np.shape(A)
     Q = np.zeros_like(A)
     R = np.zeros((cols, cols))
-
     for i in range(cols):
         q = A[:, i].copy()
         j = 0
         while j < i:
             q_j = Q[:, j].copy()
-            R[j, i] = np.dot(q, q_j)
+            R[j, i] = np.dot(A[:, i], q_j)
             q -= q_j * R[j, i]
             j += 1
         R[j, i] = np.sqrt(np.dot(q, q))
         Q[:, i] = q / R[j, i]
-
     return Q, R
 
 
