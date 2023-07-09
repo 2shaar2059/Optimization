1311b8855e5c3d030efab7bdc88bc1b8e05220ad
Tushaar <tushaar.jain.duke@gmail.com>
2023-07-08 19:31:59 -0700

    able to form tableau

--- LinearProgramming/SimplexSovler.py
+++ LinearProgramming/SimplexSovler.py
@@ -1,43 +1,82 @@
 from LinearProgram import LinearProgram
+import numpy as np
+
+"""NOTES:
+
+    basic variables >= 0
+    non-basic variabels == 0
+"""
+
 
 class Tableau:
-    def __init__(self, lp):
-        self.c = lp.c
+    def __init__(self, A, b, c):
+        num_constraints, num_variables = A.shape
+        self.num_basic_vars = num_constraints
+        self.num_nonbasic_vars = num_variables - num_constraints
+        self.matrix = np.concatenate(
+            (
+                np.concatenate((A, b), axis=1),
+                np.concatenate((c.T, [[0]]), axis=1),
+            ),
+            axis=0,
+        )
 
-        self.num_nonbasic_vars = len(lp.constraints)
-        self.num_basic_vars = lp.
-        self.
+    @property
+    def c(self):
+        return self.tableau[-1, :-1]
+
+    def unbounded(self):
+        return np.all(self.c <= 0)
+
+    def infeasible(self):
+        return True  # TODO fixme
+
+    def terminated(self):
+        return np.all(self.c >= 0)
+
+    def pivot(self, idxs):
+        """pivot to try to decrease objective
+
+        Args:
+            idxs (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        initial_objective = TODO
+
+        final_objective = TODO
+
+        assert final_objective <= initial_objective
 
 
-def initialize():
-    tableau = TODO
+# class SimplexSolver:
+#     def initialize():
+#         bfs = np.zeros_like(self.c)  # TODO initalize by solving the auxillary LP
+#         tableau = TODO
 
-    return tableau
-
-
-def pivot():
-    initial_objective = TODO
-
-    final_objective = TODO
-
-    assert final_objective >= initial_objective
-    return tableau
+#         return tableau
 
 
-def unbounded(tableau):
-    return TODO
+#     def solve(lp):
+#         tableau = initialize(lp)
+#         while not terminated(tableau):
+#             tableau = pivot(tableau)
+#         return tableau.solution()
 
 
-def infeasible(tableau):
-    return TODO
-
-
-def terminated(tableau):
-    return TODO
-
-
-def solve(lp):
-    tableau  = initialize(lp)
-    while (not terminated(tableau)):
-        tableau = pivot(tableau)
-    return tableau.solution()
+if __name__ == "__main__":
+    A = np.array(
+        [
+            [-6, 0, 1, -2, 2],
+            [3, 1, -1, 8, 1],
+        ]
+    )
+    b = np.array([[6, 9]]).T
+    c = np.array(
+        [
+            [-4, 1, 1, 7, 3],
+        ]
+    ).T
+    t = Tableau(A, b, c)
+    print(t.matrix)
